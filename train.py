9e810c835138868c89ee134263720944605353c6
Mingfei Li <ml@mingfei.io>
2024-07-22 14:56:57 -0700

    Update train.py

--- train.py
+++ train.py
@@ -1,33 +1,65 @@
-import re
 import multiprocessing
+import re
+import time
 from datasets import load_dataset
 from gensim.models import Word2Vec
+from multiprocessing import Pool
 
-ds = load_dataset('wikimedia/wikipedia', '20231101.en')['train'][:100]['text']
-corpus = [re.sub(r"[^A-Za-z'\d\-]+", " ", doc).lower().split() for doc in ds]
-vocab = sorted(list({word for doc in corpus for word in doc}))
-# word_to_index = {}
-# for index, word in enumerate(vocab):
-#     word_to_index[word] = index
+class Corpus():
+    def __init__(self):
+        self._dataset = load_dataset('wikimedia/wikipedia', '20231101.en')['train']
 
-print('before model iniitalization')
-model = Word2Vec(
-    sentences=corpus,
-    vector_size=100,
-    window=5,
-    min_count=1,
-    workers=multiprocessing.cpu_count()-1,
-)
+    def __iter__(self):
+        for doc in self._dataset:
+            sentence = re.sub(r"[^A-Za-z'\d\-]+", " ", doc['text']).lower().split()
+            yield sentence
 
-print('before build_vocab')
-model.build_vocab(corpus_iterable=corpus, progress_per=10000)
+def train(corpus):
+    print("Iniitalizing Word2Vec...")
+    model = Word2Vec(
+        sentences=corpus,
+        vector_size=100,
+        window=5,
+        min_count=1,
+        workers=multiprocessing.cpu_count()-1,
+    )
+    
+    print("Building vocab...")
+    model.build_vocab(corpus_iterable=corpus(), progress_per=10000)
+    
+    print("Training model...")
+    model.train(
+        corpus_iterable=corpus(),
+        total_examples=model.corpus_count,
+        epochs=30,
+    )
 
-print('before train')
-model.train(
-    corpus_iterable=corpus,
-    total_examples=model.corpus_count,
-    epochs=30,
-)
+    print("Saving model...")
+    model.save("word2vec.model")
+    print("Model saved")
 
-print('after train')
-eval_ds = load_dataset('tomasmcz/word2vec_analogy')
+    return model
+
+def eval(model):
+    print("Evaluating model...")
+    eval_ds = load_dataset('tomasmcz/word2vec_analogy')['train']
+    test_count = 0
+    passed = 0
+    for row in eval_ds:
+        try:
+            pred = model.wv.most_similar(positive=[row['word_b'], row['word_c']], negative=[row['word_a']])
+            top, _ = pred[0]
+            target = row['word_d']
+            if top == target:
+                passed += 1
+            test_count += 1
+            # print(f"{row['word_a']} to {row['word_b']} is as {row['word_c']} to (target: {row['word_d']}, pred: {top})")
+        except:
+            pass
+        
+    print(f'Accuracy={float(passed) / test_count}, {passed} out of {test_count} test passed')
+
+if __name__ == "__main__":
+    corpus = Corpus()
+    model = train(corpus)
+    eval(model)
