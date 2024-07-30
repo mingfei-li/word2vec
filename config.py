76efbb7aa97d0371a6b7dba78f792f87075e3b02
Mingfei Li <ml@mingfei.io>
2024-07-29 22:54:03 -0700

    wikitext-103-raw-v1 run_id=6

--- config.py
+++ config.py
@@ -3,18 +3,18 @@ import torch
 
 class Config():
     def __init__(self):
-        self.run_id = 5
+        self.run_id = 6
         self.dataset = 'Salesforce/wikitext'
         self.subset = 'wikitext-103-raw-v1'
         self.num_epochs = 3
         self.embedding_dim = 300
-        self.batch_size = 5
+        self.batch_size = 10
         self.window_size = 10
         self.neg_k = 15
         self.min_freq = 50
         self.eval_freq = 10000
         self.lr = 2.5e-2
-        self.lr_decay = 0.9999
+        self.lr_decay = 0.99999
         self.limit = None
 
         self.base_dir = f'results/{self.subset}/{self.run_id}'
