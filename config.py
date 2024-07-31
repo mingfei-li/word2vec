903ff65185b06a64f78b9b2908f5d74a35233ab1
Mingfei Li <ml@mingfei.io>
2024-07-30 21:44:51 -0700

    wikitext-103-raw-v1 run_id=12

--- config.py
+++ config.py
@@ -3,18 +3,18 @@ import torch
 
 class Config():
     def __init__(self):
-        self.run_id = 11
+        self.run_id = 12
         self.dataset = 'Salesforce/wikitext'
         self.subset = 'wikitext-103-raw-v1'
         self.vocab = 'vocab-bert-v2'
         self.num_epochs = 10
         self.embedding_dim = 300
-        self.batch_size = 1
+        self.batch_size = 10
         self.window_size = 10
         self.neg_k = 15
         self.min_freq = 50
-        self.eval_freq = 100_000
-        self.lr = 1e-3
+        self.eval_freq = 10_000
+        self.lr = 1e-2
         self.limit = None
 
         self.vocab_path = f'results/{self.subset}/{self.vocab}'
