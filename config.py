e873eba0db5f875d86bdf18452d1750a4bc9b88a
Mingfei Li <ml@mingfei.io>
2024-08-02 00:15:52 -0700

    wikitext-103-raw-v1;vocab-bert-v3;run_id=2

--- config.py
+++ config.py
@@ -3,23 +3,23 @@ import torch
 
 class Config():
     def __init__(self):
-        self.run_id = '1.0'
+        self.run_id = '2'
         # self.train_dataset = 'wikimedia/wikipedia'
         # self.train_subset = '20231101.en'
         self.train_dataset = 'Salesforce/wikitext'
-        self.train_subset = 'wikitext-2-raw-v1'
+        self.train_subset = 'wikitext-103-raw-v1'
         self.val_dataset = 'Salesforce/wikitext'
-        self.val_subset = 'wikitext-2-raw-v1'
+        self.val_subset = 'wikitext-103-raw-v1'
         self.vocab = 'vocab-bert-v3'
-        self.num_epochs = 100
+        self.num_epochs = 10
         self.embedding_dim = 300
-        self.batch_size = 1
+        self.batch_size = 100
         self.window_size = 10
         self.neg_k = 15
         self.vocab_cap = 50_000
         self.max_len = 512
         self.eval_freq = 1000
-        self.lr = 1e-3
+        self.lr = 0.025
         self.limit = None
 
         self.vocab_path = f'results/{self.train_subset}/{self.vocab}'
